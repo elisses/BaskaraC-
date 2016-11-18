3014caa88c113dd1f61b8938bf669c5c11339a17
Elis Fernanda <fernandalisses@gmail.com>
2016-11-18 13:23:15 -0200

    Baskara C#

---
+++ Baskara/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baskara
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 1, b = 12, c = -13;
+            double delta, a1, a2;
+
+            delta = b * b - 4 * a * c;
+            a1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            a2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+            MessageBox.Show("A1: " + a1);
+            MessageBox.Show("A2: " + a2);
+        }
+    }
+}
